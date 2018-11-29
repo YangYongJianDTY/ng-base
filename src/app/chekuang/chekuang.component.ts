46840f5e6978aa53424c82e1e56ad9d2a31e56e2
YangYongJianDTY <dtyzsyyyj@163.com>
2018-11-29 13:42:25 +0800

    finish basic pages

--- src/app/chekuang/chekuang.component.ts
+++ src/app/chekuang/chekuang.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, forwardRef, Inject } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, AbstractControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-chekuang',
@@ -7,9 +8,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChekuangComponent implements OnInit {
 
-  constructor() { }
+chekuangForm: FormGroup
 
-  ngOnInit() {
+constructor(@Inject(forwardRef(() => FormBuilder)) private formBuilder: FormBuilder) {
+}
+
+submit(): void {
+  console.log(this.chekuangForm.value)
+}
+
+reset(): void {
+  this.chekuangForm.reset()
+}
+
+ctrl(item: string): AbstractControl {
+  return this.chekuangForm.controls[item]
+}
+
+statusCtrl(item: string): string {
+  if (!this.chekuangForm.controls[item]) return
+  const control: AbstractControl = this.chekuangForm.controls[item]
+  return control.dirty && control.hasError('status') ? control.errors.status : ''
+}
+
+messageCtrl(item: string): string {
+  if (!this.chekuangForm.controls[item]) return
+  const control: AbstractControl = this.chekuangForm.controls[item]
+  return control.dirty && control.hasError('message') ? control.errors.message : ''
+}
+
+
+ngOnInit(): void {
+  this.chekuangForm = this.formBuilder.group({
+    chexing: [ '' ],
+    licheng: [ '' ],
+    youliang: [ '' ],
+    shuiliang: [ '' ],
+    shuiwen: [ '' ],
+    qidong: [ '' ],
+    wending: [ '' ],
+    xiangsheng: [ '' ],
+    zhuanxiang: [ '' ],
+    shache: [ '' ],
+    lihe: [ '' ],
+    biansuxiang: [ '' ],
+    chedeng: [ [] ],
+  })
+}
+
+private timeValidator = (control: FormControl): Validators => {
+  if (!control.value) {
+    return { status: 'error', message: '必须选择配送时间' }
   }
+  return { status: 'success' }
+}
+
+private dateValidator = (control: FormControl): Validators => {
+  if (!control.value) {
+    return { status: 'error', message: '必须选择配送日期' }
+  }
+  return { status: 'success' }
+}
+
 
 }
